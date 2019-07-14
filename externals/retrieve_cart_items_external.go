2a5ce324eda30cc9f5cb2f0bcea4a60e733af870
ilhammhdd <milham939@gmail.com>
2019-07-14 10:45:14 +0700

    switched to single consumer mode

--- externals/retrieve_cart_items_external.go
+++ externals/retrieve_cart_items_external.go
@@ -22,6 +22,6 @@ func (rci *RetrieveCartItems) Work() interface{} {
 		inTopics:           []string{events.RentalServiceCommandTopic_RETRIEVE_CART_ITEMS.String()},
 		outTopic:           events.RentalServiceEventTopic_CART_ITEMS_RETRIEVED.String()}
 
-	ede.handle()
+	ede.handleSingleConsumer()
 	return nil
 }
--- externals/update_cart_item_external.go
+++ externals/update_cart_item_external.go
@@ -25,7 +25,7 @@ func (uci *UpdateCartItem) Work() interface{} {
 		inTopics:            []string{events.RentalServiceCommandTopic_UPDATE_CART_ITEM.String()},
 		outTopic:            events.RentalServiceEventTopic_CART_ITEMS_UPDATED.String()}
 
-	ede.handle()
+	ede.handleSingleConsumer()
 	return nil
 }
 
